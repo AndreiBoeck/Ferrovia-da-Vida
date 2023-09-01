4bb88f7a83a62cd19e0e809527e798210a054ee7
marcelazarichta <141656198+marcelazarichta@users.noreply.github.com>
2023-09-01 16:24:48 -0300

    Add files via upload

---
+++ src/main/java/garagemLocomotivas.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class garagemLocomotivas
+{
+    ArrayList <Locomotivas> garagemLocomotivas = new ArrayList <Locomotivas>();
+    
+    public void addLocomotiva(Locomotivas locomotiva)
+    {
+        garagemLocomotivas.add(locomotiva);
+    }
+    
+    public void removeLocomotiva(Locomotivas locomotiva)
+    {
+        garagemLocomotivas.remove(locomotiva);
+    }
+    
+    public int getLocomotivasNaGaragem()
+    {
+        return garagemLocomotivas.size();
+    }
+}
