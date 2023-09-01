8050c12ce8a285692cd038af29fd6d4128111b27
Lambari <78892876+AndreiBoeck@users.noreply.github.com>
2023-08-31 22:26:12 -0300

    add "Vagões" and "GaragemVagoes" for furder implementations

---
+++ src/main/java/GaragemVagoes.java
@@ -0,0 +1,3 @@
+public class GaragemVagoes {
+    public int
+}
--- src/main/java/Vagão.java
+++ src/main/java/Vagão.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Vagão {
+    private int ID;
+    private double capacity;
+    public boolean availability;
+
+    public int getID() {
+        return ID;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+    public ArrayList<String> getComposition(){
+        ArrayList<String> compositon = new ArrayList<>();
+        return compositon;
+    }
 }
