6126cbdb9dc5d6acc2d16ca441db735e36d8a00f
Roth <rafael2rooth@gmail.com>
2023-09-04 12:54:46 -0300

    metodos para criar id e nome(nask)(nask)

--- src/main/java/GaragemLocomotivas.java
+++ src/main/java/GaragemLocomotivas.java
@@ -2,16 +2,25 @@ import java.util.ArrayList;
 
 public class GaragemLocomotivas
 {
+
+    private int v = 0;
+    private int atual = 0;
     ArrayList <Locomotiva> garagemLocomotivas = new ArrayList <Locomotiva>();
-    
+
+    //mudar capacidade maxima dependendo da capacidade
+
     public void addLocomotiva(Locomotiva locomotiva)
     {
         garagemLocomotivas.add(locomotiva);
+        garagemLocomotivas.get(atual).setNome(v);
+        v++;
+        atual++;
+
     }
     
     public void removeLocomotiva(Locomotiva locomotiva)
     {
-        garagemLocomotivas.remove(locomotiva);
+        garagemLocomotivas.remove(atual);
     }
     
     public int getLocomotivasNaGaragem()
